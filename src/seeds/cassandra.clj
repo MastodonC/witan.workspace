175bcae882e5ab08a42ff9005851f0df8e7799b7
Antony Woods <antony@mastodonc.com>
2016-06-03 17:03:40 +0100

    Fix + explanation per @otfrom comment

--- src/seeds/cassandra.clj
+++ src/seeds/cassandra.clj
@@ -3,8 +3,7 @@
             [qbits.hayt                 :as hayt]
             [joplin.cassandra.database :refer [get-connection]]))
 
+;; leaving this here for reference
 (defn run [target & args]
   (let [conn (get-connection (-> target :db :hosts)
-                             (-> target :db :keyspace))]
-    #_(alia/execute conn
-                    (hayt/insert "users" (hayt/values {:id "Kalle" :email "ole@dole.doff"})))))
+                             (-> target :db :keyspace))]))

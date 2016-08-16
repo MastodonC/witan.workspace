e03124df9fdcd209bccd0143a24c0eb4bee63f25
Antony Woods <antony@mastodonc.com>
2016-08-16 14:57:19 +0100

    Simplify the data functions concerning coercion

--- src/witan/workspace/data.clj
+++ src/witan/workspace/data.clj
@@ -22,31 +22,18 @@
                   (s/one datatype fieldname)))
         (:column-names col-schema)))
 
-(defn record-coercion
-  "Coerce numbers by matching them to the
-    type specified in the schema"
-  [schema data]
-  (let [coerce-data-fn
-        (coerce/coercer schema
-                        coerce/string-coercion-matcher)]
-    (coerce-data-fn data)))
-
 (defn apply-row-schema
   [col-schema data]
   (let [row-schema (make-row-schema col-schema)]
-    (map #((partial (fn [s r] (record-coercion s r)) row-schema) %) data)))
+    (map (coerce/coercer row-schema coerce/string-coercion-matcher) data)))
 
 (defn apply-col-names-schema
   [col-schema headers]
   (let [col-names-schema (make-col-names-schema col-schema)]
-    (record-coercion col-names-schema headers)))
-
-(defn create-dataset-after-coercion
-  [{:keys [column-names columns]}]
-  (ds/dataset column-names columns))
+    ((coerce/coercer col-names-schema coerce/string-coercion-matcher) headers)))
 
 (defn ->dataset
   [headers data schema]
-  (create-dataset-after-coercion
-   {:column-names (apply-col-names-schema schema (custom-keyword headers))
-    :columns (vec (apply-row-schema schema data))}))
+  (ds/dataset
+   (apply-col-names-schema schema (custom-keyword headers))
+   (vec (apply-row-schema schema data))))
